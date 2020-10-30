33592ca6f789721b5f5046be9ad89948ec911158
devrajn <nik8470@gmail.com>
2020-10-29 21:42:41 -0400

    some more minor edits; still incomplete

--- population.py
+++ population.py
@@ -1,22 +1,121 @@
 import numpy as np
 from copy import deepcopy
 
-class SIR_Env:
-    def __init__(self):
-        pass
+"""
+Most of the following is taken from https://github.com/mit-gfx/PGMORL/, with minor adaptations
+"""
+def predict_hyperbolic(args, opt_graph, optgraph_id, test_weights):
+    """
+    Taken from https://github.com/mit-gfx/PGMORL/
+    Receives: optimization graph, policy identifier, weights for test run
+    Outputs: results, which is a dict with policy and predictions for that policy
+    Effects: Trains hyperbolic prediction function for a policy of optgraph_id.
+    """
+    test_weights = np.array(test_weights)
 
-    def step(self, action):
-        pass
+    # normalize the test_weights to be sum = 1
+    for test_weight in test_weights:
+        test_weight /= np.sum(test_weight)
+    
+    threshold = 0.1
+    sigma = 0.03
+    # gradually enlarging the searching range so that get enough data point to fit the model
+    while True:
+        objs_data, weights_data, delta_objs_data = collect_nearest_data(opt_graph, optgraph_id, threshold)
+        cnt_data = 0
+        for i in range(len(weights_data)):
+            flag = True
+            for j in range(i):
+                if np.linalg.norm(weights_data[i] - weights_data[j]) < 1e-5:
+                    flag = False
+                    break
+            if flag:
+                cnt_data += 1
+                if cnt_data > 3:
+                    break
+        if cnt_data > 3:
+            break
+        else:
+            threshold *= 2.0
+            sigma *= 2.0
 
-    def reset(self):
-        pass
+    def f(x, A, a, b, c):
+        return A * (np.exp(a * (x - b)) - 1) / (np.exp(a * (x - b)) + 1) + c
 
+    def fun(params, x, y):
+        # f = A * (exp(a(x - b)) - 1) / (exp(a(x - b)) + 1) + c
+        return (params[0] * (np.exp(params[1] * (x - params[2])) - 1.) / (np.exp(params[1] * (x - params[2])) + 1) + params[3] - y) * w
+
+    def jac(params, x, y):
+        A, a, b, c = params[0], params[1], params[2], params[3]
+
+        J = np.zeros([len(params), len(x)])
+
+        # df_dA = (exp(a(x - b)) - 1) / (exp(a(x - b)) + 1)
+        J[0] = ((np.exp(a * (x - b)) - 1) / (np.exp(a * (x - b)) + 1)) * w
+
+        # df_da = A(x - b)(2exp(a(x-b)))/(exp(a(x-b)) + 1)^2
+        J[1] = (A * (x - b) * (2. * np.exp(a * (x - b))) / ((np.exp(a * (x - b)) + 1) ** 2)) * w
+
+        # df_db = A(-a)(2exp(a(x-b)))/(exp(a(x-b)) + 1)^2
+        J[2] = (A * (-a) * (2. * np.exp(a * (x - b))) / ((np.exp(a * (x - b)) + 1) ** 2)) * w
+
+        # df_dc = 1
+        J[3] = w
+
+        return np.transpose(J)
+
+    M = args.obj_num
+    delta_predictions = []
+    for dim in range(M):
+        train_x = []
+        train_y = []
+        w = []
+        for i in range(len(objs_data)):
+            train_x.append(weights_data[i][dim])
+            train_y.append(delta_objs_data[i][dim])
+            diff = np.abs(objs_data[i] - opt_graph.objs[optgraph_id])
+            dist = np.linalg.norm(diff / np.abs(opt_graph.objs[optgraph_id]))
+            coef = np.exp(-((dist  / sigma) ** 2) / 2.0)
+            w.append(coef)
+        
+        train_x = np.array(train_x)
+        train_y = np.array(train_y)
+        w = np.array(w)
+
+        A_upperbound = np.clip(np.max(train_y) - np.min(train_y), 1.0, 500.0)
+        params0 = np.ones(4)
+        
+        f_scale = 20.
+
+        # fit the prediction function by minimizing soft_l1 loss.
+        res_robust = least_squares(
+            fun,
+            params0,
+            loss='soft_l1',
+            f_scale = f_scale,
+            args = (train_x, train_y),
+            jac = jac,
+            bounds = ([0, 0.1, -5., -500.], [A_upperbound, 20., 5., 500.])
+        )
+        
+        delta_predictions.append(f(test_weights.T[dim], *res_robust.x))
+
+    predictions = []
+    delta_predictions = np.transpose(np.array(delta_predictions))
+    original_objs = opt_graph.objs[optgraph_id]
+    for i in range(len(test_weights)):
+        predictions.append(original_objs + delta_predictions[i])
+
+    results = {'sample_index': optgraph_id, 'predictions': predictions}
+
+    return results
+    
 
 class Population:
     def __init__(self):
         self.population = []
         self.models = []
-
 
     def compute_hypervolume(self, objs_batch):
         """
@@ -73,17 +172,43 @@ class Population:
                 sparsity[i] = self.compute_sparsity(new_objs_batch)     
         return sparsity        
 
-    def prediction_model_candidates():
+    def prediction_model_candidates(args, opt_graph):
         """
-        Receives:
+        Receives: 
         Outputs: candidate (policy, weight) pairs to use in task selection
         """
         # Prediction model used for calculation of expected objectives (see eq. (5) in paper)
-
+        num_weights = args.num_weights_candidates
+        
+        candidates = []
+        for policy in self.policy_sample_batch:
+            weight_center = opt_graph.weights[policy.optgraph_id]
+            angle_center = np.arctan2(weight_center[1], weight_center[0])
+            angle_bound = [angle_center - np.pi / 4., angle_center + np.pi / 4.]
+            test_weights = []
+            for i in range(num_weights):
+                angle = angle_bound[0] + (angle_bound[1] - angle_bound[0]) / (num_weights - 1) * i
+                weight = np.array([np.cos(angle), np.sin(angle)])
+                if weight[0] >= -1e-7 and weight[1] >= -1e-7:
+                    duplicated = False
+                    for succ in opt_graph.succ[policy.optgraph_id]: # discard duplicate tasks
+                        w = deepcopy(opt_graph.weights[succ])
+                        w = w / np.linalg.norm(w)
+                        if np.linalg.norm(w - weight) < 1e-3:
+                            duplicated = True
+                            break
+                    if not duplicated:
+                        test_weights.append(weight)
+            if len(test_weights) > 0:
+                results = predict_hyperbolic(args, opt_graph, policy.optgraph_id, test_weights)
+                for i in range(len(test_weights)):
+                    candidates.append({'policy': policy, 'weight': test_weights[i], \
+                        'prediction': results['predictions'][i]})
+        
         return candidates
 
         
-    def prediction_guided_task_selection(num_tasks, ep, scalarization_template):
+    def prediction_guided_task_selection(args, ep, scalarization_template):
         """
         Receives: number of tasks to select `num_tasks` and pareto archive `ep` (see paper)
         Outputs: selected tasks `selected_tasks`
@@ -91,6 +216,7 @@ class Population:
 
         candidate_tasks = self.prediction_model_candidates()
         num_cands = len(candidate_tasks)
+        num_tasks = args.num_tasks
 
         # Task Selection (see Algorithm 3 in paper)
         task_mask = np.ones(num_cands, dtype=bool)
